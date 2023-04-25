81370cc9ad96b784a26dedf0c4cf453ad0bf8ac6
Morteza Sabihi <morteza.sabihi@gmail.com>
2023-04-25 16:44:54 +0330

    chore(site): add seo title

--- site/pages/_document.tsx
+++ site/pages/_document.tsx
@@ -4,7 +4,9 @@ import { roboto } from "../styles/theme";
 export default function Document() {
   return (
     <Html lang="en" className={roboto.className}>
-      <Head />
+      <Head>
+        <title>Iconsans - +320 Free Icons for your next project</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
