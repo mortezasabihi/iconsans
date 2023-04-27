f80c52861d613a031dfcb3d360913adb5950e4a7
Morteza Sabihi <morteza.sabihi@gmail.com>
2023-04-27 22:36:15 +0330

    ci(site): add NODE_ENV to build step

--- site/next.config.mjs
+++ site/next.config.mjs
@@ -1,9 +1,13 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
   output: "export",
   distDir: "dist",
+  basePath: isProd ? "/iconsans" : undefined,
+  assetPrefix: isProd ? "/iconsans" : undefined,
 };
 
 export default nextConfig;
