e6c752fbb7b66d62291e6fb8a3f14f4c7c2d213d
Morteza Sabihi <morteza.sabihi@gmail.com>
2023-05-02 18:52:07 +0330

    chore(react): add missing src directory

--- packages/react/remove-react-import.cjs
+++ packages/react/remove-react-import.cjs
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-const componentsDir = path.join(__dirname, "components");
+const componentsDir = path.join(__dirname, "src/components");
 
 fs.readdir(componentsDir, (err, files) => {
   if (err) {
