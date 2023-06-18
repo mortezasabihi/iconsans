0b131794cc3e46160aae281626f957687fa5892c
Morteza Sabihi <morteza.sabihi@gmail.com>
2023-06-18 17:13:12 +0330

    fix(site): update favicon in production

--- site/pages/_document.tsx
+++ site/pages/_document.tsx
@@ -2,11 +2,16 @@ import { Html, Head, Main, NextScript } from "next/document";
 import { roboto } from "../styles/theme";
 
 export default function Document() {
+  const favIcon =
+    process.env.NODE_ENV === "production"
+      ? "/iconsans/favicon.ico"
+      : "/favicon.ico";
+
   return (
     <Html lang="en" className={roboto.className}>
       <Head>
         <title>Iconsans - +320 Free Icons for your next project</title>
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href={favIcon} />
       </Head>
       <body>
         <Main />
