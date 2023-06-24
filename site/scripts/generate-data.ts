0c3f94de184c1ac4ac821864b03ee6cacac7d777
Morteza Sabihi <morteza.sabihi@gmail.com>
2023-06-24 14:20:51 +0330

    chore(site): update generate-data script to match the new icons directory strucutre

--- site/scripts/generate-data.ts
+++ site/scripts/generate-data.ts
@@ -8,24 +8,39 @@ function kebabToPascal(kebabCase: string): string {
     return pascalCase.replaceAll(' ', '');
 }
 
+function findFiles(dir: string, fileList: string[] = []) {
+    const files = fs.readdirSync(dir);
+    files.forEach((file) => {
+        const filePath = path.join(dir, file);
+        const stat = fs.statSync(filePath);
+
+        if (stat.isDirectory()) {
+            findFiles(filePath, fileList);
+        } else if (path.extname(file) === '.svg') {
+            fileList.push(filePath);
+        }
+    });
+
+    return fileList;
+}
 
 (async () => {
     try {
         const directoryPath = path.join(process.cwd(), '../icons');
-        const files = await fs.promises.readdir(directoryPath);
+        const files = findFiles(directoryPath)
 
         const fileList = files
-            .filter((file) => file.endsWith('.svg'))
             .map((file) => {
-                const fileName = file.replace('.svg', '');
+                const filePathArr = file.split('/')
+                const fileName = filePathArr.pop().replace('.svg', '');
                 const camelCaseName = kebabToPascal(fileName.toLowerCase());
-                const filePath = path.join(directoryPath, file);
-                const fileContent = fs.readFileSync(filePath, 'utf-8').replace(`width=\"24\"`, `width=\"50\"`)
+                const fileContent = fs.readFileSync(file, 'utf-8').replace(`width=\"24\"`, `width=\"50\"`)
                     .replace(`height=\"24\"`, `height=\"50\"`);
 
                 return {
                     name: camelCaseName,
                     svg: fileContent,
+                    style: filePathArr.slice(-1)[0].toLowerCase()
                 };
             });
 
